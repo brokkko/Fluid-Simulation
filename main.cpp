9474f2e9bc2adb8d552c74bbd0ad9d3d6df2867b
Алексей Печёркин <lexa.pech@yandex.ru>
2022-04-05 09:45:42 +0300

    fix github

--- main.cpp
+++ main.cpp
@@ -1,181 +1,45 @@
-#include <iostream>
 #include <SFML/Graphics.hpp>
-
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include "src/Graphics.h"
+#include "src/Grid.h"
+#include "src/Simulation.h"
 // dU/dt = a*dU/dx
-
-struct Cell
-{
-    double val;
-
-    Cell operator+(const Cell r) const
-    {
-        return Cell{ val + r.val };
-    }
-    Cell operator-(const Cell r) const
-    {
-        return Cell{ val - r.val };
-    }
-    Cell operator*(const Cell r) const
-    {
-        return Cell{ val * r.val };
-    }
-    Cell operator/(const Cell r) const
-    {
-        return Cell{ val / r.val };
-    }
-    Cell operator*(const double r) const
-    {
-        return Cell{ val * r };
-    }
-    Cell operator/(const double r) const
-    {
-        return Cell{ val / r };
-    }
-    friend Cell operator*(const double l, const Cell r)
-    {
-        return { l * r.val };
-    }
-};
-
-
-struct Grid
-{
-    Cell* mesh;
-    unsigned int size;
-    Grid(unsigned int size)
-    {
-        this->size = size;
-        mesh = new Cell[size+1];  
-        mesh[0] = { 0 };
-        mesh[size] = { 0 };
-    }
-    void Fill(double v)
-    {
-        for (int i = 0; i < size + 1; i++)
-            mesh[i].val = v;
-    }
-    void Print()
-    {
-        for (int i = 0; i < size + 1; i++)
-            std::cout << mesh[i].val<< " ";
-        std::cout <<"\n";
-    }
-    ~Grid()
-    {
-        delete[] mesh;
-    }
-};
-
-// one variable!!
-// ospre flux limiter
-Cell SlopeLim(Cell r)
-{  
-    if (r.val > 0)
-        return Cell{ 1.5 * (r.val * r.val + r.val) / (r.val * r.val + r.val + 1) };
-    return Cell{ 0 };
-}
-
-Cell F(Cell u)
-{
-    double a = 50;
-    return u * a;
-}
-
-//one variable !!
-Cell nonZeroDenom(Cell denom)
-{
-    if (abs(denom.val) < __DBL_EPSILON__)
-    {
-        if (denom.val < 0)
-            return {-__DBL_EPSILON__};
-        else 
-            return {__DBL_EPSILON__};
-    }
-    return denom;
-}
-
-void CalculateFlux(Grid& out, Grid& in)
-{
-    //|  E  |  P  |  W  |
-    //      e     p 
-    //in.Print();
-    for (int i = 2; i < in.size; i++) // 2->n ??
-    {   
-        //r_i= (u{i} - u{i-1}) / (u{i+1}-u{i})   r_P = (P - E) / (W - P)
-        auto ri =   (in.mesh[i]     - in.mesh[i - 1]) / nonZeroDenom(in.mesh[i + 1] - in.mesh[i]);
-        auto ri_1 = (in.mesh[i - 1] - in.mesh[i - 2]) / nonZeroDenom(in.mesh[i] - in.mesh[i - 1]);
-
-        //uL{i-0.5} = u{i-1} + 0.5 * phi(r{i-1}) * (u{i} - u{i - 1})
-        //uL{P} = E + 0.5 * phi * (P - E)
-        //uR{i-0.5} = u{i} - 0.5 * phi(r{i}) * (u{i+1} - u{i})
-        //uR{P} = P - 0.5 * phi * (W - P)
-        //phi - slope limiter
-        auto uL = in.mesh[i - 1] + 0.5 * SlopeLim(ri_1) * (in.mesh[i] - in.mesh[i - 1]);
-        auto uR = in.mesh[i]     - 0.5 * SlopeLim(ri)   * (in.mesh[i + 1] - in.mesh[i]);
-
-        //eigenvalues? -> max speed a
-        double a = 50;
-        // F{i-0.5} = 0.5 * (F(uR{i-0.5}) + F(uL{i-0.5}) - a * (uR{i-0.5} - uL{i-0.5}))
-        out.mesh[i] = 0.5 * (F(uR) + F(uL) - a * (uR - uL));
-    }
-    out.mesh[0] = { 0 };
-    out.mesh[1] = { 0 };
-}
-
-
-void RKIntegrator(Grid& grid, double dt)
-{
-    double dx = 1;
-    //f=f(grid)
-    //k1 = -dt/dx * (f(u){i+0.5} - f(u){i-0.5}
-    //k2=f(grid+0*5*k1*dt)
-    //grid=grid+k2*dt
-    Grid flux(grid.size);
-    CalculateFlux(flux,grid);
-    Grid k(grid.size);
-    for (int i = 1; i < grid.size; i++)
-    {
-        k.mesh[i] = grid.mesh[i] - 0.5 * dt / dx * (flux.mesh[i + 1] - flux.mesh[i]);
-    }
-    CalculateFlux(flux, k);
-    for (int i = 1; i < grid.size; i++)
-    {
-        grid.mesh[i] = grid.mesh[i] - dt / dx * (flux.mesh[i + 1] - flux.mesh[i]);
-    }
-}
-
-
-void show(Grid& grid, sf::RenderWindow& window) {
-    sf::Vertex* line=new sf::Vertex[grid.size];
-
-    int windowsize = window.getSize().x/2;
-    int segment = window.getSize().x / grid.size;
-    for (int i = 0; i < grid.size; i++)
-    {
-        line[i] = sf::Vertex(sf::Vector2f(double(i* segment)- windowsize, -grid.mesh[i].val));
-       
-    }
-    window.draw(line, grid.size, sf::LineStrip);
-    delete[] line;
-}
 
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1000, 600), "wave");
-    
-    Grid grid(1000);
-    grid.Fill(0);
-    for (int i = 10; i < 300; i++) grid.mesh[i].val = i % 50 > 25 ? 200 : -200;
-   //grid.mesh[10].val = 50;
-    //grid.mesh[11].val = 50;
-   // dataArray[20] = 50;
-    //dataArray[80] = 50;
-    double h = 0.01;
+    sf::RenderWindow window(sf::VideoMode(700, 700), "wave", sf::Style::Default, sf::ContextSettings(32));
+    window.setActive(true);
+    window.setFramerateLimit(60);
+    //InitGL();
+    Grid grid(70,70);
+    InitialConditions(grid);
+    //grid.Fill(0.01);
+    /*for (int y = 45; y < 55; y++){
+        for (int x = 20; x < 40; x++){
+            grid.mesh[x][y].rho=5;
+            grid.mesh[x][y].vx=10;
+            grid.mesh[x][y].vy=0;
+            grid.mesh[x][y].Bz=0;
+            grid.mesh[x][y].E = 3000;
+        }
+    }*/
+
+    double h = 0.001;
     sf::View w;
     w = window.getDefaultView();
     w.setCenter(0, 0);
     window.setView(w);
+    sf::Font f;
+    f.loadFromFile("arial.ttf");
+
+    sf::Text t;
+    t.setFont(f);
+    t.setCharacterSize(12);
+    t.setFillColor(sf::Color::Magenta);
+
     while (window.isOpen())
     {
         sf::Event event;
@@ -184,21 +48,28 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+
         window.clear(sf::Color::Black);
-        //solve(dataArray, arraySize, h);
-        RKIntegrator(grid, h);
+        for(int i=0;i<1;i++)
+            RKIntegrator(grid, h);
         double sum = 0;
-        for (int i = 0; i < grid.size; i++)
+        double vel = 0;
+        for (int y = 0; y < grid.sizeY; y++)
         {
-            sum += abs(grid.mesh[i+1].val - grid.mesh[i].val);
+            for (int x = 0; x < grid.sizeX; x++){
+                sum += grid.mesh[x][y].E;
+               // vel+=grid.mesh[x][y].E);
+
+            }
         }
-        printf("Total Variation = %0.10e\n",sum);
-       // std::cout << dataArray[5] << "\n";
-        show(grid, window);
+        printf("E = %0.10e\n",sum);
+        // printf("vel = %0.10e\n",vel);
+
+        show(grid, window,t);
 
         window.display();
     }
 
     return 0;
 }
-
---
+++ src/Graphics.cpp
@@ -0,0 +1,85 @@
+#include "Graphics.h"
+#define COLOR_SCHEME 0
+sf::Color toColor(double val,double min,double max)
+{
+
+    double mid = (min+max)/2;
+    if (std::isnan(val)){
+        return sf::Color::Magenta;
+    }
+#if COLOR_SCHEME == 0
+    double r = std::min(1.0, 1 - (mid - val) / (max - mid));
+    double g = 1 - std::abs(val - mid) / (max - mid);
+    double b = std::min(1.0, 1 - (val - mid) / (max - mid));
+#else
+    double n = (val - min) / (max - min);
+
+    double r=-5*n*n+7.5*n+1-5.0/1.777777777;
+    double g=-20*n*n+20*n-4;
+    double b=-5*n*n+2.5*n+0.6875;
+#endif
+    sf::Color c((int)std::max(0.0,r*255),(int)std::max(0.0,g*255),(int)std::max(0.0,b*255));
+    return c;
+}
+
+void show(Grid& grid, sf::RenderWindow& window,sf::Text& t) {
+    //sf::Vertex* points=new sf::Vertex[grid.sizeX*grid.sizeY];
+    sf::Vertex varr[grid.sizeX];
+    sf::Vertex varry[grid.sizeY];
+    sf::Vertex l[2*((grid.sizeX+1)/5)*((grid.sizeY+1)/5+2)];
+    double graphm=1.0/3;
+    int graph_h=100;
+
+    auto mPos=sf::Mouse::getPosition()- window.getPosition();
+
+    unsigned int windowsizeX = window.getSize().x;
+    unsigned int windowsizeY = window.getSize().y;
+    int segmentX = windowsizeX / grid.sizeX;
+    int segmentY = (windowsizeY-graph_h) / grid.sizeY;
+    int mposx=std::clamp(mPos.x/segmentX,0,(int)grid.sizeX);
+    int mposy=std::clamp(mPos.y/segmentY,0,(int)grid.sizeY);
+
+    sf::RectangleShape r;
+    r.setSize(sf::Vector2f{float(segmentX),float(segmentY)});
+    for (int y = 0; y < grid.sizeY+1; y++){
+        for (int x = 0; x < grid.sizeX+1; x++){
+            double radius=0.05;
+
+            Cell U =grid.mesh[x][y];
+            double gamma = 5.0/3 - 1;
+            double p = gamma * (U.E - 1.0/2*U.rho* (U.vx*U.vx + U.vy*U.vy + U.vz*U.vz)
+                                - 1.0/2*(U.Bx*U.Bx + U.By*U.By + U.Bz*U.Bz));
+            double P = p + 1.0/2*(U.Bx*U.Bx + U.By*U.By + U.Bz*U.Bz);
+
+            auto displayvar=U.rho;
+            r.setFillColor(toColor(displayvar,-radius*0,radius));
+            r.setPosition(sf::Vector2f(  float(x* segmentX)- (float)windowsizeX/2, float(y* segmentY)- (float)windowsizeY/2));
+            window.draw(r);
+            if (x%5 ==0 && y % 5 ==0)
+            {
+                auto vec=sf::Vector2f(  float(x* segmentX)- (float)windowsizeX/2, float(y* segmentY)- (float)windowsizeY/2);
+                sf::Vector2f dir={(float)grid.mesh[x][y].vx*0.9f,(float)grid.mesh[x][y].vy*0.9f};
+                l[2*(x/5+y/5*grid.sizeX/5)] = sf::Vertex(vec,sf::Color::Black);
+                l[2*(x/5+y/5*grid.sizeX/5)+1]=  sf::Vertex(vec+dir,sf::Color::Black);
+
+            }
+            if (y==mposy)
+                varr[x] = sf::Vertex(sf::Vector2f(float(x* segmentX)/2- (float)windowsizeX/2,(float)windowsizeY/2-graph_h/2-grid.mesh[x][y].E*graphm ),sf::Color::Green);
+        }
+
+        if (y==mposy)
+        {
+            window.draw(varr, grid.sizeX, sf::LinesStrip);
+        }
+        varry[y] = sf::Vertex(sf::Vector2f(float(y* segmentX)/2,(float)windowsizeY/2-graph_h/2-grid.mesh[mposx][y].E*graphm ),sf::Color::Green);
+    }
+    window.draw(l, 2*((grid.sizeX)/5)*((grid.sizeY)/5), sf::Lines);
+    window.draw(varry, grid.sizeY, sf::LinesStrip);
+
+    std::stringstream ss;
+
+    ss<<grid.mesh[mposx][mposy].E;
+    t.setString(ss.str());
+    t.setPosition(window.mapPixelToCoords( {mPos.x,mPos.y}));
+    window.draw(t);
+}
